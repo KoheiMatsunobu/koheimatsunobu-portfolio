930e36999860b6690ab09840e02e6545904d4272
KoheiMatsunobu <koheimatsunobuengineer@gmail.com>
2021-07-16 22:38:19 +0900

    Add Google reCAPTCHAv3

---
+++ libs/grecaptcha_execute.php
@@ -0,0 +1,24 @@
+<?php
+require './libs/recapthcavars.php';
+
+$gscore = 0.8;
+
+if (isset($_POST['recaptchaResponse']) && !empty($_POST['recaptchaResponce'])) {
+    $secret = V3_SECRETKEY;
+    $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret=' . $secret . '&response=' . $_POST['recaptchaResponse']);
+
+    $responseData = json_decode($verifyResponse);
+
+    if ($responseData->success) {
+        if ($responseData->score < $gscore) {
+            // 認証スコアが低いのでbotの可能性
+            exit();
+        }
+    } else {
+        // 認証失敗した場合
+        exit();
+    }
+} else {
+    // POST値が正常に投げられていなかった場合
+    exit();
+}
