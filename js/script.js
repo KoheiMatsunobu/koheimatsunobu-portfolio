9fcd741545087af733c7dc6aade6f9c252f74a5b
KoheiMatsunobu <koheimatsunobuengineer@gmail.com>
2022-08-07 01:46:21 +0900

    Add Splash Loading

--- js/script.js
+++ js/script.js
@@ -420,7 +420,7 @@ $(function () {
         }
       },
       "opacity": {
-        "value": 0.664994832269074,
+        "value": 0.2,
         "random": false,
         "anim": {
           "enable": true,
@@ -475,5 +475,24 @@ $(function () {
       }
     },
     "retina_detect": true
+  });
+
+  // 画面ロード時背景変更
+  $(window).on('load', function () {
+    $("#splash-logo").delay(1200).fadeOut('slow');//ロゴを1.2秒でフェードアウトする記述
+
+    //=====ここからローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
+    $("#splash").delay(1500).fadeOut('slow', function () {//ローディングエリア（splashエリア）を1.5秒でフェードアウトする記述
+
+      $('body').addClass('appear');//フェードアウト後bodyにappearクラス付与
+
+    });
+    //=====ここまでローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
+
+    //=====ここから背景が伸びた後に動かしたいJSをまとめたい場合は
+    $('.splashbg').on('animationend', function () {
+      //この中に動かしたいJSを記載
+    });
+    //=====ここまで背景が伸びた後に動かしたいJSをまとめる
   });
 });
